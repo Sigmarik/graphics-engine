18d31d5e568141a76ef5a942ada8bc8bce78fde9
sigmarik <itkudryashov@gmail.com>
2024-09-18 10:22:06 +0300

    feat: `StaticBoxField` structure refactor: `LevelGeometry` to use `StaticBoxField` instead of a self-implemented structure

--- lib/physics/level_geometry.h
+++ lib/physics/level_geometry.h
@@ -9,23 +9,19 @@
  *
  */
 
-#ifndef PHYS_GRID_H
-#define PHYS_GRID_H
+#pragma once
 
 #include <glm/vec3.hpp>
 #include <vector>
 
 #include "collider.h"
 #include "geometry/primitives.h"
-
-typedef size_t collider_id_t;
+#include "geometry/static_box_field.hpp"
 
 using CollisionGroup = std::vector<BoxCollider>;
 
 struct LevelGeometry {
     LevelGeometry(Box bounding_box, size_t horiz_res, size_t vert_res);
-    LevelGeometry(const LevelGeometry& geometry);
-    ~LevelGeometry();
 
     LevelGeometry& operator=(const LevelGeometry& geometry);
 
@@ -34,30 +30,5 @@ struct LevelGeometry {
     void add_collider(const BoxCollider& collider);
 
    private:
-    struct LevelCell {
-        std::vector<collider_id_t> colliders = {};
-        Box box = Box();
-    };
-
-    struct IndexBox {
-        size_t low_x = 0, high_x = 0;
-        size_t low_y = 0, high_y = 0;
-        size_t low_z = 0, high_z = 0;
-    };
-
-    IndexBox find_indices(const Box& box) const;
-
-    size_t horiz_res_;
-    size_t vert_res_;
-
-    Box boundary_;
-
-    size_t get_cell_id(size_t id_x, size_t id_y, size_t id_z) const;
-
-    LevelCell* cells_ = nullptr;
-
-    std::vector<collider_id_t> global_colliders_ = {};
-    std::vector<BoxCollider> colliders_ = {};
+    StaticBoxField<BoxCollider> colliders_;
 };
-
-#endif
